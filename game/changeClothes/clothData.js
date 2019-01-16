9c8b4f6b69902d49bfb3141cdced05129fd0ef44
Shopping <emilyisneko@gmail.com>
2019-01-16 10:52:20 +0800

    change order

--- game/changeClothes/clothData.js
+++ game/changeClothes/clothData.js
@@ -4,20 +4,25 @@ const clothData = {
         'x':145, 'y': 50,
         'w': 200, 'h': 200
     },
+    'sad':{
+        'img': 'Picture15.png',
+        'x':143, 'y': 90,
+        'w': 200, 'h': 100
+    },
     'Lolipop':{
         'img': 'Picture4.png',
         'x':300, 'y': 50,
         'w': 200, 'h': 200
     },
-    'hat1':{
-        'img': 'Picture6.png',
-        'x':200, 'y': 0,
-        'w': 120, 'h': 120
-    },
     'pipe':{
         'img': 'Picture7.png',
         'x':85, 'y': 130,
         'w': 100, 'h': 100
+    },
+    'hat1':{
+        'img': 'Picture6.png',
+        'x':200, 'y': 0,
+        'w': 120, 'h': 120
     },
     'cowboy':{
         'img': 'Picture8.png',
@@ -34,15 +39,35 @@ const clothData = {
         'x':145, 'y': -82,
         'w': 250, 'h': 250
     },
-    'sad':{
-        'img': 'Picture15.png',
-        'x':143, 'y': 90,
-        'w': 200, 'h': 100
+    'NOSE':{
+        'img': '鼻子.png',
+        'x':121, 'y': 37,
+        'w': 250, 'h': 150
     },
     'mustash':{
         'img': 'Picture17.png',
         'x':210, 'y': 130,
         'w': 70, 'h': 70
+    },
+    '鬍子':{
+        'img': '鬍子.png',
+        'x':210, 'y': 113,
+        'w': 70, 'h': 70
+    },
+    'necklace':{
+        'img': 'Picture45.png',
+        'x':132, 'y': 85,
+        'w': 223, 'h': 255
+    },
+    '領結':{
+        'img': '領結.png',
+        'x':105, 'y': 140,
+        'w': 285, 'h': 150
+    },
+    'earing':{
+        'img': '右耳環.png',
+        'x':125, 'y': 42,
+        'w': 150, 'h': 100
     },
     'fakehair':{
         'img': 'Picture26.png',
@@ -64,11 +89,6 @@ const clothData = {
         'x':65, 'y': 35,
         'w': 400, 'h': 255
     },
-    'shirt':{
-        'img': 'Picture34.png',
-        'x':95, 'y': 185,
-        'w': 298, 'h': 200
-    },
     'fakeghair1':{
         'img': 'Picture38.png',
         'x':118, 'y': 30,
@@ -84,15 +104,15 @@ const clothData = {
         'x':118, 'y': 65,
         'w': 260, 'h': 255
     },
-    'necklace':{
-        'img': 'Picture45.png',
-        'x':132, 'y': 85,
-        'w': 223, 'h': 255
+    'fakeghair4':{
+        'img': '爆炸頭.png',
+        'x':95, 'y': 0,
+        'w': 300, 'h': 150
     },
-    'earing':{
-        'img': '右耳環.png',
-        'x':125, 'y': 42,
-        'w': 150, 'h': 100
+    'shirt':{
+        'img': 'Picture34.png',
+        'x':95, 'y': 185,
+        'w': 298, 'h': 200
     },
     '背心':{
         'img': '背心(右).png',
@@ -108,25 +128,5 @@ const clothData = {
         'img': '游泳圈.png',
         'x':65, 'y': 215,
         'w': 355, 'h': 150
-    },
-    '領結':{
-        'img': '領結.png',
-        'x':105, 'y': 140,
-        'w': 285, 'h': 150
-    },
-    'NOSE':{
-        'img': '鼻子.png',
-        'x':121, 'y': 37,
-        'w': 250, 'h': 150
-    },
-    'fakeghair4':{
-        'img': '爆炸頭.png',
-        'x':95, 'y': 0,
-        'w': 300, 'h': 150
-    },
-    '鬍子':{
-        'img': '鬍子.png',
-        'x':210, 'y': 113,
-        'w': 70, 'h': 70
     }
 }
